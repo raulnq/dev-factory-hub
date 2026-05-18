023499007977c076715146006978a39947533eaa
Raul Naupari <raulnq@gmail.com>
2026-05-18 15:23:56 -0500

    fix(frontend): suppress eslint rule and remove baseurl from tsconfig

--- apps/frontend/src/hooks/use-mobile.ts
+++ apps/frontend/src/hooks/use-mobile.ts
@@ -1,10 +1,11 @@
+/* eslint-disable react-hooks/set-state-in-effect */
 import * as React from 'react';
 
 const MOBILE_BREAKPOINT = 768;
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState(
-    () => window.innerWidth < MOBILE_BREAKPOINT
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
+    undefined
   );
 
   React.useEffect(() => {
@@ -13,8 +14,9 @@ export function useIsMobile() {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
     };
     mql.addEventListener('change', onChange);
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
     return () => mql.removeEventListener('change', onChange);
   }, []);
 
-  return isMobile;
+  return !!isMobile;
 }
